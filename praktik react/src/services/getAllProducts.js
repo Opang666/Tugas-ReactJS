7c68e0ef8707301b38dde8d1138441e49f98b981
Naufal Ferdian <naufalferdian99@gmail.com>
2024-11-17 16:20:57 +0700

    first commit

---
+++ praktik react/src/services/getAllProducts.js
@@ -0,0 +1,59 @@
+export default function getAllProducts() {
+  return [
+    {
+      id: 'ASD-1-07-FMEN-001',
+      slug: 'aerostreet-dunk-09-men',
+      name: "Aerostreet Dunk",
+      category: 'Men\'s Shoe',
+      price: 200_000,
+      stock: 100,
+      imageUrl: '/assets/images/Aerostreet Dunk.jpeg',
+      description: `Sepatu Aerostreet Dunk adalah sepatu olahraga dengan desain yang sederhana namun elegan.`
+    },
+
+    {
+      id: 'ASJ-1-07-FMEN-LV8-002',
+      slug: 'aerostreet-joker-1-10-men',
+      name: "Aerostreet Dunk ft Joker",
+      category: 'Men\'s Shoe',
+      price: 250_000,
+      stock: 0,
+      imageUrl: `/assets/images/Aerostreet Joker.jpeg`,
+      description: `Sepatu Aerostreet Joker adalah sepatu limited edition dengan desain yang sangat mewah namun elegan.`
+    },
+
+    {
+      id: 'ASL-1-07-FWOMEN-003',
+      slug: 'aerostreet-low-1-11-women',
+      name: "Aerostreet Fomz",
+      category: 'Women\'s Shoe',
+      price: 230_000,
+      stock: 5,
+      imageUrl: `/assets/images/Aerostreet Doraemon.jpeg`,
+      description: `Sepatu Aerostreet Low Doraemon adalah sepatu collab dengan kartun Doraemon dengan desain yang begitu nyentrik yang disukai bagi para penggemar kartun Doraemon.`
+    },
+
+    {
+      id: 'AHL-1-07-FMEN-004',
+      slug: 'aerostreet-Hoops-Low-1-12-men',
+      name: "Aerostreet Hoops Low",
+      category: 'Men\'s Shoe',
+      price: 180_000,
+      stock: 80,
+      imageUrl: `/assets/images/Aerostreet Hoops Low.jpeg`,
+      description: `Sepatu Aerostreet Hoops Low adalah sepatu yang sangat simpel tetapi elegan untuk anak muda.`
+    },
+
+    {
+      id: 'ASM-1-07-FMEN-005',
+      slug: 'aerostreet-Masive-1-13-men',
+      name: "Aerostreet Masive",
+      category: 'Men\'s Shoe',
+      price: 150_000,
+      stock: 200,
+      imageUrl: `/assets/images/Aerostreet Masive.jpeg`,
+      description: `Sepatu Aerostreet Masive adalah sepatu yang cocok untuk anak sekolah dengan desain yang simpel minimalis dan elegan.`
+    },
+
+  ]
+}
